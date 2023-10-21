de71e23d0c1ba26dbc51c2ee0f7e401885ec42af
Marek Jagiełło <mjagiello@ibpm.pro>
2023-10-21 19:57:06 +0200

    add task 1

--- src/main/java/interview/task/installation/InstallationRepository.java
+++ src/main/java/interview/task/installation/InstallationRepository.java
@@ -1,7 +1,6 @@
 package interview.task.installation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface InstallationRepository extends JpaRepository<InstallationEntity, Long> {
 }
--- src/main/java/interview/task/product/ProductRepository.java
+++ src/main/java/interview/task/product/ProductRepository.java
@@ -1,8 +1,6 @@
 package interview.task.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 }
